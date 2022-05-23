77ae1d75081251b41e3bb6cb3f003458c0a86dbc
Sejal Agrawal <sejalagrawal@Sejals-MacBook-Air.local>
2022-05-23 19:33:29 +0530

    deploy

---
+++ public/precache-manifest.299a822f4a6ba1d35ab0149fc81114ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e083ad3794a826e188fe81872f7b9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6df00f81496dc5426830",
+    "url": "/static/css/main.65013ead.chunk.css"
+  },
+  {
+    "revision": "fb460e7b6a572753b966",
+    "url": "/static/js/2.ede2299f.chunk.js"
+  },
+  {
+    "revision": "6df00f81496dc5426830",
+    "url": "/static/js/main.e1120a83.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
